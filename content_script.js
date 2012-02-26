077860f3b9f66a23ad28de13571b7cecea1dcd3f
Wesley Zhao <wesley.zhao@gmail.com>
2012-02-26 13:10:39 -0800

    added email submit and photo checkboxes

--- content_script.js
+++ content_script.js
@@ -52,8 +52,35 @@ function show_photos_for_copy(){
     var html = generate_perma_photos_html();
     var photos_el = generate_perma_photos_el(html);
     var lightbox_el = document.getElementById('light');
+
     lightbox_el.appendChild(photos_el);
+
+    // bind select all toggle
+    document.getElementById('select-all-toggle').addEventListener("click", toggle_select_all)
+    // end
+    toggle_select_all();
+
     show_lightbox();
+}
+
+function toggle_select_all(){
+    var select_str = document.getElementById('select-all').textContent;
+    var select_form = document.getElementById('photos-form');
+
+    var select_form_list = select_form.photo;
+    if (select_str == 'Select'){
+	for (var i=0; i<select_form_list.length; i++){
+	    select_form_list[i].checked = true;
+	}
+	document.getElementById('select-all').textContent = 'Deselect';
+    }
+    else{
+	// else it should be 'Deselect'	
+	for (var i=0; i<select_form_list.length; i++){
+	    select_form_list[i].checked = false;
+	}
+	document.getElementById('select-all').textContent = 'Select';
+    }
 }
 
 function show_lightbox(){
@@ -93,18 +120,60 @@ function generate_perma_photos_html(){
 	    thumb_src = image_src.replace("_n.jpg", "_a.jpg");
 	}
 
-	html_src += "<div><a style='margin: 5px 5px 5px 5px; float:left; height: 100px; width: 100px; overflow:hidden; position:relative; border: 1px solid black;' target='_blank' href='" + image_src +"'><img style='position:absolute; float:left;' width='140' src='" +thumb_src+"'/></a></div>";
+	html_src += "<div><a style='margin: 5px 5px 5px 5px; float:left; height: 100px; width: 100px; overflow:hidden; position:relative; border: 1px solid black;' target='_blank' href='" + image_src +"'><img style='position:absolute; float:left;' width='145' src='" +thumb_src+"'/><input style='position:absolute;' type='checkbox' name='photo' value='"+thumb_src+"'/></a></div>";
     }
     return html_src;
 }
 
 function generate_perma_photos_el(html_text){
 
+    var form_div = document.createElement('form');
+    var submit = create_button('submit', 'submit');
+    var email_span = document.createElement('div');
+    var email_input = document.createElement('input');
+    var select_all_span = document.createElement('span');
+
+    // set email input
+    email_input.setAttribute('name', 'email');
+    email_input.setAttribute('type', 'text');
+    email_input.id='email-input';
+    email_span.innerHTML = "Email photos to: ";
+    email_span.appendChild(email_input);
+
+    // set select all span
+    select_all_span.style.cursor = 'pointer';
+    select_all_span.id = "select-all-toggle";
+    select_all_span.innerHTML = "<a>(<span id='select-all'>Select</span> all photos)</a>";
+
+    form_div.setAttribute('target', '_blank');
+    form_div.setAttribute('action', 'http://weavetogether.com/form/test');
+    form_div.setAttribute('method', 'post');
+    form_div.setAttribute('onsubmit',"return (!document.getElementById('email-input').value=='')");
+    form_div.id = "photos-form";
+
+    // append stuf to form
+    email_span.appendChild(submit);
+    form_div.appendChild(email_span);
+    form_div.appendChild(select_all_span);
+    // form_div.appendChild(submit);
+
     var div = document.createElement('div');
     div.id = "fb-thumbs";
     var msg = "Try highlighting all the photos and copy/pasting into an email!<br>";
+    msg = "";
     div.innerHTML = msg+html_text;
-    return div;
+    form_div.appendChild(div);
+    return form_div;
+}
+
+function check_photos_form(){ 
+    if(document.getElementById('email-input').value==''){
+	console.log('here');
+	return false;
+    }
+    else{
+	return true;
+    }  
 }
 
 function send_mailgun_mail(){
